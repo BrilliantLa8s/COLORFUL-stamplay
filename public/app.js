6576fa9b2ffa8fae57b74fb54a52e1ec18b997b8
Ricky Brown <ricky.brown@indigoslate.com>
2016-10-14 21:15:57 -0700

    remove build files from git

--- public/app.js
+++
@@ -1,4 +0,0 @@
-angular.module("app",["ui.router"]);const app=angular.module("app");app.run(["$rootScope","$state",function(a,o){a.$state=o,console.log("app is running!")}]);
-
-app.config(["$locationProvider","$stateProvider","$urlRouterProvider",function(e,o,r){e.html5Mode(!0).hashPrefix("!"),o.state("home",{url:"/",template:"<home></home>"})}]);
-app.directive("home",function(){return{restrict:"E",templateUrl:"home/home.html",controller:["$scope",function(e){e.message="Hi man, this is the home page"}]}});
--- public/templates.js
+++
@@ -1,1 +0,0 @@
-angular.module('app').run(['$templateCache', function($templateCache) {$templateCache.put('home/home.html','<div>{{message}}</div>\r\n');}]);
